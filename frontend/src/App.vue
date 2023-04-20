2bea74d4a9ff5cd54c5d90db4b1311c3c52845af
Duc Nguyen <nguyenvanducwork@gmail.com>
2023-04-20 20:09:52 +0700

    Frontend: Add admin page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,31 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="navbar navbar-expand-sm bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">FaceID</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <router-link to="admin" class="nav-link">
+              <span>Admin</span>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="#" class="nav-link">
+              <span>Face Detection</span>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="#" class="nav-link">
+              <span>Face Recognition</span>
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
   </nav>
+
   <router-view/>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
---
+++ frontend/src/views/AdminView.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <button class="btn btn-primary my-3">Register New Person</button>
+
+    <div class="row row-cols-1 row-cols-sm-1 row-cols-md-3 g-3">
+
+      <div class="col" v-for="(person, i) in peoples" :key="i">
+        <div class="card">
+          <img src="https://source.unsplash.com/featured" alt="..." class="card-img-top img-fluid"
+            style="object-fit: cover; height: 20rem;">
+          <div class="card-body">
+            <h5 class="card-title">Nguyen Van Duc</h5>
+            <h6 class="card-subtitle text-muted ">nguyenvanducwork@gmail.com</h6>
+            <div class="btn-group mt-2" role="group" aria-label="Basic example">
+              <button type="button" class="btn btn-outline-primary">Edit</button>
+              <button type="button" class="btn btn-outline-primary">Photos</button>
+              <button type="button" class="btn btn-outline-secondary">Disable</button>
+              <button type="button" class="btn btn-outline-danger">Delete</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      peoples: [
+        {
+          id: 0, full_name: 'Nguyen Van Duc', email: 'nguyenvanducwork@gmail.com'
+        },
+        {
+          id: 0, full_name: 'Nguyen Van Duc', email: 'nguyenvanducwork@gmail.com'
+        },
+        {
+          id: 0, full_name: 'Nguyen Van Duc', email: 'nguyenvanducwork@gmail.com'
+        },
+        {
+          id: 0, full_name: 'Nguyen Van Duc', email: 'nguyenvanducwork@gmail.com'
+        },
+        {
+          id: 0, full_name: 'Nguyen Van Duc', email: 'nguyenvanducwork@gmail.com'
+        },
+        {
+          id: 0, full_name: 'Nguyen Van Duc', email: 'nguyenvanducwork@gmail.com'
+        },
+        {
+          id: 0, full_name: 'Nguyen Van Duc', email: 'nguyenvanducwork@gmail.com'
+        }
+      ]
+    }
+  }
+}
+</script>
