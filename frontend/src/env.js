4cb3f75080bf1661508b703a51d7a56b6225e4c1
Duc Nguyen <nguyenvanducwork@gmail.com>
2023-05-04 17:28:14 +0700

    Frontend: Containerize

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -1,9 +1,4 @@
-// require('dotenv').config()
-
-// export const settings = {
-//   API_DOMAIN: process.env.API_DOMAIN
-// }
-
+// FIXME: this file is run at build stage, it does not know about environment variable that are added by docker-compose
 export const settings = {
-  API_DOMAIN: 'http://localhost:8080'
+  API_DOMAIN: process.env.API_DOMAIN || 'http://localhost:8080'
 }
